da584d0509d451eab57705e677d8107383af962f
Enio Fernandes <ef@cic.ac>
2018-02-06 15:39:58 +0000

    head

--- web.js
+++ web.js
@@ -3,5 +3,6 @@ var fs = require('fs')
 
 http.createServer(function (req, res) {
     text = fs.readFileSync('insert.html', 'utf8')
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(text)
 }).listen(process.env.PORT)
